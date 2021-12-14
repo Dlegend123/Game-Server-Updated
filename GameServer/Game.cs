a8070669d1a8bd50e8ecedf1c3264e90cae65129
MARK MORRISON <MARKPMORRISON@students.utech.edu.jm>
2021-12-13 23:29:09 -0500

    initial commit

--- GameServer/Game.cs
+++ GameServer/Game.cs
@@ -11,42 +11,8 @@
 
         public T[] GetTop10Players()
         {
-            int[] highScores = new int[10];
-
-            for (int i = 0; i < 10; i++)
-            {
-                highScores[i] = _players[i].Score;
-            }
-            for (int i = 1; i <= 9; i++)
-            {
-                for (int j = 1; j <= 9 - 1; j++)
-                {
-                    if (highScores[j] > highScores[j + 1])
-                    {
-                        (highScores[j + 1], highScores[j]) = (highScores[j], highScores[j + 1]);
-                    }
-                }
-            }
-
-            for (int i = 1; i <= _players.Count; i++)
-            {
-                for (int j = 1; j <= _players.Count - 1 - 1; j++)
-                {
-                    if (_players[j].Score > _players[j + 1].Score)
-                    {
-                        (_players[j + 1].Score, _players[j].Score) = (_players[j].Score, _players[j + 1].Score);
-                    }
-                }
-            }
-            int r = 0;
-            T[] topPlayers = new T[10];
-            for (int i = _players.Count - 1; i >= _players.Count - 10; i--)
-            {
-                topPlayers[r] = _players[i];
-                r++;
-            }
             // ... write code that returns 10 players with highest scores
-            return topPlayers;
+            return _players.OrderByDescending(x=>x.Score).Take(10).ToArray();
         }
         
     }
--- GameServer/Startup.cs
+++ GameServer/Startup.cs
@@ -64,7 +64,7 @@ namespace GameServer
             Console.ReadKey();
             */
             //Assignment 2
-            /*
+            
             List<Player> players = GameHelpers.InstPlayers();
             players = GameHelpers.SetPlayerData(players);
 
@@ -82,7 +82,8 @@ namespace GameServer
                 Console.WriteLine("Player {0}", i);
                 GameHelpers.printItem(players[i]);
             }
-          */
+          
+            /*
             //Assignment 3
             var builder = WebApplication.CreateBuilder(args);
 
@@ -133,6 +134,7 @@ namespace GameServer
                 endpoints.MapControllers();
             });
             app.Run();
+            */
         }
     }
 }
